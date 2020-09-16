8a2ed3082299b1a8ab30e45d7869f2ace1227bbb
Trouvaille0198 <1375911011@qq.com>
2020-09-16 10:58:42 +0800

    add work10

--- week2/wk10.cpp
+++ week2/wk10.cpp
@@ -12,11 +12,50 @@ int Sum(int n)
     return sum;
 }
 
+int cmpInt(const void *a, const void *b)
+{
+    return Sum(*(int *)a) - Sum(*(int *)b);
+}
+int cmpDouble(const void *a, const void *b)
+{
+    return *(double *)a - *(double *)b;
+}
+int cmpChar(const void *a, const void *b)
+{
+    int size1 = strlen(*(char **)a);
+    int size2 = strlen(*(char **)b);
+    if (size1 > size2)
+        return 1;
+    else if (size1 == size2)
+        return strcmp(*(char **)a, *(char **)b);
+    else
+        return -1;
+}
+
 int main()
 {
     int a[10] = {12, 32, 42, 51, 8, 16, 51, 21, 19, 9};
     double b[10] = {32.1, 456.87, 332.67, 442.0, 98.12, 451.79, 340.12, 54.55, 99.87, 72.5};
-    string c[10] = {"enter", "number", "size", "begin", "of", "cat", "case", "program", "certain", "a"};
-    getchar();
-    getchar();
+    char *c[10] = {"enter", "number", "size", "begin", "of", "cat", "case", "program", "certain", "a"};
+    /*  Sort int */
+    qsort(a, 10, sizeof(int), cmpInt);
+    for (int i = 0; i < 10; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    /* Sort double */
+    qsort(b, 10, sizeof(double), cmpDouble);
+    for (int i = 0; i < 10; i++)
+    {
+        cout << b[i] << " ";
+    }
+    cout << endl;
+    /* Sort string */
+    qsort(c, 10, sizeof(*c), cmpChar);
+    for (int i = 0; i < 10; i++)
+    {
+        cout << c[i] << " ";
+    }
+    system("pause");
 }

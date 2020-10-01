37547eaca32a2476dbae526ba685b21acbcc5ce6
KetchupLover <yyzztao@outlook.com>
2020-10-01 10:56:09 +0800

    update of works for week2

--- week4/work2/week4-2.cpp
+++ week4/work2/week4-2.cpp
@@ -7,20 +7,17 @@ struct  PointList
     Point p;
     PointList* next;
 };
-void insertHead(PointList*& head)
+void insertHead(PointList*& head,Point newp)
 {
-    int* a = new int[5];
-    int* b = new int[5];
     PointList* q;
-    q = (PointList*)malloc(sizeof(PointList));
-    q->next = nullptr;
-    cout << "输入横坐标:"<< endl;
-    cin >>*a;
-    cout << "输入纵坐标:" << endl;
-    cin >> *b;
-    head->p= Point(*a, *b);
+   q = (PointList*)malloc(sizeof(PointList));
+    //PointList *q = new PointList;
+    q->p = newp;
     if (head->next == NULL)//原先为空链表
+    {
         head->next = q;
+        q->next = NULL;
+    }
     else
     {
         q->next = head->next;
@@ -29,19 +26,29 @@ void insertHead(PointList*& head)
 }//插入
 void displayList(PointList*& head)
 {
-    PointList* q = head;
-    while (q != NULL&&q->next !=NULL)
+    PointList* q = head->next;
+    while (q != NULL)
     {
         q->p.Show();
         q = q->next;
     }
 }//显示
+Point p1(1, 1);
 int main()
 {
+    Point p2(2, 2);
+    Point arrayp[3] = { Point(3,3), Point(4,4),Point(5,5) };
+    Point* p = new Point(6, 6);
     PointList* head;
     head = (PointList*)malloc(sizeof(PointList));
     head->next = NULL;
-    insertHead(head);
+    insertHead(head, p1);//插入全局量
+    insertHead(head, p2);//插入局部量
+    for (int i = 0; i < 3; i++)
+    {
+        insertHead(head, arrayp[i]);//插入数组
+    }
+    insertHead(head, *p);//插入指针
     displayList(head);
 }
 
